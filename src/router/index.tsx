eed12379e6ae02427c6a388446dab0dc586386e2
Cloudly Le <vanlethi.dev@gmail.com>
2026-06-27 22:10:13 +0700

    fix: React Router basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,17 +3,20 @@ import HomePage from '../pages/HomePage'
 import { LoginPage } from '../pages/LoginPage'
 import { NotFoundPage } from '../pages/NotFoundPage'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '*',
-    element: <NotFoundPage />,
-  },
-])
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/login',
+      element: <LoginPage />,
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />,
+    },
+  ],
+  { basename: '/Cloudlyme/' },
+)
